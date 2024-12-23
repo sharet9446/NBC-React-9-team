9abb02ff871d85e631517c3ad2a3670f039bc36d
sharet9446 <spees97@naver.com>
2024-12-23 23:58:22 +0900

    로또 번호 추첨기

--- lotto-generator/script.js
+++ lotto-generator/script.js
@@ -1,14 +1,24 @@
 document.getElementById('generateButton').addEventListener('click', function () {
-    let numbers = generateLottoNumbers();
-    document.getElementById('numbers').textContent = numbers.join(', ');
-})
+    const numbersElement = document.getElementById('numbers');
+    const prevNumbersElement = document.getElementById('prevNumbers');
+
+    if (numbersElement.textContent) {
+        prevNumbersElement.textContent = `이전 번호: ${numbersElement.textContent}`;
+    }
+    
+    const numbers = generateLottoNumbers();
+    numbersElement.textContent = numbers.join(', ');
+});
+
 
 function generateLottoNumbers() {
-    let LottoNumbers = []
-    for (let i = 0; i < 6; i++) {
-        let randomNumber = (Math.floor(Math.random() * 45) + 1)
-        LottoNumbers.includes(randomNumber) ? i -= 1
-            : LottoNumbers.push(randomNumber)
-    }
-    return LottoNumbers.sort((a, b) => a - b)
-}
+	const lottoNumbers = []; 
+    while(lottoNumbers.length < 6){    
+        const randomNumber = Math.floor(Math.random() * 45) + 1; 
+        if(!lottoNumbers.includes(randomNumber)){ 
+            lottoNumbers.push(randomNumber); 
+        };
+    };
+    return lottoNumbers.sort((a,b)=>a-b);
+};
+
